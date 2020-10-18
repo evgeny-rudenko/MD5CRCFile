fc96220adacb7318a8e8135c573d73bd4844445e
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2020-10-18 09:44:01 +0300

    first commit

---
+++ md5file/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Reflection;
+namespace md5file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length==1)
+            {
+                if (File.Exists(args[0]))
+                {
+                    string filenamewoe = Path.GetFileNameWithoutExtension(args[0]);
+                    string filename = args[0];
+                    string mdhash = CalculateMD5(filename);
+
+                    File.WriteAllText(filenamewoe+ ".crc", mdhash);
+                }
+            }
+            else
+            {
+                string executablepath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                DirectoryInfo d = new DirectoryInfo(executablepath);
+                FileInfo[] Files = d.GetFiles(); 
+                string str = "";
+                foreach (FileInfo file in Files)
+                {
+                    string extension = Path.GetExtension(file.Name);
+                    if (extension != ".rar")
+                        continue;
+                    if (!File.Exists(Path.GetFileNameWithoutExtension(file.Name)+".crc"))
+                    {
+                        string mdhash = CalculateMD5(file.Name);
+                        File.WriteAllText(Path.GetFileNameWithoutExtension(file.Name) + ".crc", mdhash);
+                    }
+                }
+            }
+        
+        }
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToUpperInvariant();
+                }
+            }
+        }
+
+    }
+}
---
+++ md5file/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("md5file")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("md5file")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5257b28a-96b1-4246-a237-45e5de9578b9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.7.7")]
+[assembly: AssemblyFileVersion("1.2020.1008.1942")]
+
+[assembly: AssemblyInformationalVersion("1.0.7")]
